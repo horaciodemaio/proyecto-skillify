f0cd1e3e9fbd53332e2e7854a20430f529c7933a
horaciodemaio <horaciodemaio33@gmail.com>
2022-01-03 20:49:34 -0300

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,3 @@
+export default function IndexPage(){
+    return <p className="text-2xl text-green-600 font-bold">Hello world</p>
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    mode: 'jit',
+    purge: ['./src/**/*.js'],
+    darkMode: false,
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
