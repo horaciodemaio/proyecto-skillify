834554710fd6ba5460f79a29b1e025d2954af9b6
horaciodemaio <horaciodemaio33@gmail.com>
2022-01-03 21:07:07 -0300

    configuracion boton y indexpage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,25 @@
-export default function IndexPage(){
-    return <p className="text-2xl text-green-600 font-bold">Hello world</p>
+import Image from 'next/image'
+import { Button } from '../components/button'
+
+export default function IndexPage() {
+  return (
+    <div className="flex flex-col m-8 max-w-md">
+      <h2 className="mb-6 text-2xl font-bold">Confirm and pay</h2>
+      <Button className="bg-[#01B1EA] flex justify-center items-center">
+        Pay with{' '}
+        <span className="ml-1">
+          <Image
+            height={40}
+            width={98}
+            src="/img/mercadopago.png"
+            alt="Mercado Pago"
+          />
+        </span>
+      </Button>
+      <div className="my-6 relative flex justify-center items-center">
+        <span className="text-center bg-white px-4">Or pay with card</span>
+        <div className="w-full h-[1px] z-[-1] bg-gray-300 absolute top-[13px]" />
+      </div>
+    </div>
+  )
 }
